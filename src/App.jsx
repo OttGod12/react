a1df7c7e5e6247557368bb01e3056921634f4289
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2024-08-21 11:00:12 -0300

    base
    base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+export function App() {
+
+  return(
+    <>
+      <h1>Titulo</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, est, natus facilis eius dolores consequuntur officia provident cumque maiores quisquam, enim molestias quo perferendis praesentium maxime doloribus culpa eligendi nam?</p>
+
+    
+    </>
+  )
+}
+
+
+
+
+
+  
