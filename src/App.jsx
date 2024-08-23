4082d716a2b0166f8138f5277531de8f48edcba2
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2024-08-23 11:50:36 -0300

    agr deu certo
    foi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Header } from './components/Header.jsx'
-import { Main } from './components/Main.jsx'
-import { Footer } from './components/Footer.jsx'
+import { Header } from './components/Header/Header.jsx'
+import { Main } from './components/Main/Main.jsx'
+import { Footer } from './components/Footer/Footer.jsx'
 
 
 
@@ -8,8 +8,11 @@ export function App(){
   return(
     <>
       <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <Main>
+        <h3>conteúdo</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi pariatur dolorum, dolorem ipsam esse aut, officia quis ullam at laudantium consectetur eveniet. Quaerat eos, doloribus alias excepturi tempore debitis eveniet!</p>
+      </Main>
+      <Footer autor="Otávio" ></Footer>
     </>
   
   )
